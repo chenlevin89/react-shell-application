547d8070854d0904620f07d254f887094cce8119
chen.levin <chen.levin@ironsrc.com>
2023-02-01 04:33:36 +0200

    stable with mount callback

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import React, {useState, useEffect, Suspense, lazy} from 'react';
+import {BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import {observable} from '@chenlevin89/awesome-lib'
 import DynamicLoad from './components/DynamicLoad';
+import Navbar from './components/Navbar';
+import {activities, createNewActivity, counter} from './state';
+import './App.css';
+import '@chenlevin89/awesome-lib/dist/index.css'
+
+
+// Lazy Routes
+const Home = lazy(() => import('./components/Home'));
+const Activities = lazy(() => import('./components/Activities'));
+const Operations = lazy(() => import('reactRemote/Operations'));
+
 
 function App() {
+
+  const [state, setState] = useState({counter, activities});
+
+  const callback = (value) => {
+    const activity = createNewActivity(value);
+    const activities = [...state.activities, activity];
+    setState({...state, counter: state.counter + value, activities})
+  };
+
+  useEffect(() => {
+    observable.register({eventName: 'update_balance', callback});
+  });
+
+  useEffect(() => {
+    observable.unregister({eventName: 'update_balance', callback});
+  }, [])
+
+
   return (
     <Router>
       <div className="App">
@@ -8,10 +40,17 @@ function App() {
         <div className="container">
           <Suspense fallback={<div>Loading...</div>}>
             <Switch>
+              <Route exact path="/">
+                <Home counter={state.counter} />
+              </Route>
+              <Route path="/activities">
+                <Activities activities={state.activities} />
+              </Route>
+              <Route path="/operations">
+                <Operations />
+              </Route>
               <Route path="/contact">
-                <DynamicLoad
-                  dynamicImport={() => import('vueRemote/CreateApp')}
-                  props={{email: "atm-app@js.com"}} />
+                <DynamicLoad dynamicImport={() => import('vueRemote/CreateApp')} props={{email: "atm-app@js.com"}} />
               </Route>
             </Switch>
           </Suspense>
@@ -20,5 +59,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
